58f27262da3bb827be3da1521dd132e8f4daff3a
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-10 15:53:59 +0300

    working project

---
+++ 4.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter А ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter C ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+
+            Console.WriteLine($"Your result {Max(a, b, c)}");
+        }
+
+        public static int Max(int a, int b, int c)
+        {
+            return Math.Max(a * b * c, a + b + c) + 3;
+        }
+
+    }
+}
